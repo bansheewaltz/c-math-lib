07dfd44b7dc064ba50fda579c561706ec62983a7
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-05-02 16:13:51 +0700

    fmod unnecessary checks removed

--- src/lib/s21_fmod.c
+++ src/lib/s21_fmod.c
@@ -3,32 +3,8 @@
 #include "s21_fp_utils.h"
 #include "s21_math.h"
 
-// long double s21_fmod(double x, double y) {
-//   if (!s21_isinf(x) && s21_isinf(y)) {
-//     return x;
-//   }
-
-//   double quotient = s21_trunc(x / y);
-//   long double without_trace = y * quotient;
-//   long double result = x - without_trace;
-
-//   return result;
-// }
-
 long double s21_fmod(double x, double y) {
-  if (s21_isnan(y)) {
-    return S21_FP_NAN;
-  }
-  if (y == 0) {
-    return S21_FP_NAN;
-  }
-  if (x == 0) {
-    return 0;
-  }
-  if (s21_isinf(x) && s21_isinf(y)) {
-    return S21_FP_NAN;
-  }
-  if (s21_isinf(y)) {
+  if (!s21_isinf(x) && s21_isinf(y)) {
     return x;
   }
   if (x == DBL_MAX && y == DBL_MIN) {
