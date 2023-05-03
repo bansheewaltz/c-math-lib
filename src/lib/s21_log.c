bec5d23ce008c194507398a1622ac003e15c69d6
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-05-03 16:46:49 +0700

    log passed all the tests

--- src/lib/s21_log.c
+++ src/lib/s21_log.c
@@ -1,52 +1,45 @@
-// #include "./utils/s21_fp_utils.h"
-// #include "./include/s21_math.h"
-
-// #include <stdio.h>
-// #include <math.h>
+#include <stdbool.h>
 
 #include "s21_fp_utils.h"
 #include "s21_math.h"
 
-long double s21_log(double x) {
-  long double y = 0;
-  if (x < 0 || x != x) {
-    y = S21_FP_NAN;
-  } else if (x == 0) {
-    y = -S21_FP_PINF;
-  } else if (x == S21_FP_PINF) {
-    y = S21_FP_PINF;
-  } else if (x < 1) {
-    long double rez_temp = 1;
-    long double rez = 1;
-    int i = 1;
-    int i_sight = 1;
-    while (rez || -rez > S21_EPSILON) {
-      if (i % 2 == 0) {
-        i_sight = -1;
-      } else {
-        i_sight = 1;
-      }
-      rez_temp = rez_temp * (x - 1);
-      rez = i_sight * rez_temp / i;
-      y += rez;
-      i++;
-    }
-  } else {
-    long double rez = 1;
-    int i = 0;
-    while (i < 1000) {  //????????????
-      rez = 2 * (x - s21_exp(y)) / (x + s21_exp(y));
-      y += rez;
-      i++;
-    }
-  }
-  return y;
-}
+#define INITIAL_GUESS 0.5L
 
-// int main(){
-//     double x = 0;
-//     scanf("%lf", &x);
-//     printf("x = %lf, s21_log = %Lf\n", x, s21_log(x));
-//     printf("x = %lf,     log = %f\n", x, log(x));
-//     return 0;
-// }
+// through Newton-Raphson method
+long double s21_log(double x) {
+  if (s21_isnan(x) || x < 0) {
+    return S21_FP_NAN;
+  }
+  if (s21_ispinf(x)) {
+    return S21_FP_PINF;
+  }
+  if (x == 0) {
+    return S21_FP_NINF;
+  }
+
+  bool input_is_smaller_than_one = x < 1.0L;
+  if (input_is_smaller_than_one) {
+    x = 1.0L / x;
+  }
+
+  size_t e_exponent_whole_part_count = 0;
+  while (x >= S21_E) {
+    x /= S21_E;
+    ++e_exponent_whole_part_count;
+  }
+
+  long double ith_term = INITIAL_GUESS;
+  long double partial_sum = ith_term;
+
+  while (s21_fabs(ith_term) > S21_EPSILON) {
+    ith_term = 2 * (x - s21_exp(partial_sum)) / (x + s21_exp(partial_sum));
+    partial_sum += ith_term;
+  }
+  partial_sum += e_exponent_whole_part_count;
+
+  if (input_is_smaller_than_one) {
+    partial_sum *= -1;
+  }
+
+  return partial_sum;
+}
