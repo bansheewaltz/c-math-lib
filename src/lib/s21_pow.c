52b80b6fdc44dbeed5a026b767f89467d5cfccd9
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-05-04 13:06:31 +0700

    pow: 15 precision errors

--- src/lib/s21_pow.c
+++ src/lib/s21_pow.c
@@ -2,25 +2,56 @@
 #include "s21_math.h"
 
 long double s21_pow(double base, double exp) {
-  long double y = 0;
-  int sight = 1;
-  int exp_temp = exp;
-  if (base == 0 && exp < 0) {
-    y = S21_FP_PINF;
-  } else if (base == 0 && exp > 0) {
-    y = 0;
-  } else if ((base == 0 && exp == 0) || base == 1) {
-    y = 1;
-  } else if (base != base || exp != exp ||
-             (base < 0 &&
-              exp - exp_temp != 0)) {  // Проверка на NAN сделать функцию
-    y = S21_FP_NAN;
-  } else {
-    if (base < 0) {
-      base = -base;
-      if ((exp - exp_temp == 0) && exp_temp % 2 != 0) sight = -1;
-    }
-    y = sight * s21_exp(exp * s21_log(base));
+  if (base == 1.0 || exp == 0.0) {
+    return 1.0L;
   }
-  return y;
+  if (base == -1.0) {
+    if (s21_isinf(exp)) {
+      return 1.0L;
+    }
+    if (s21_fabs(exp) == DBL_MAX) {
+      return 1.0L;
+    }
+  }
+  if (base == 0.0) {
+    if (s21_ispositive(base) && exp < 0) {
+      return S21_FP_PINF;
+    }
+
+    if (s21_isnegative(base) && exp < 0) {
+      if (s21_isint(exp) && s21_isodd(exp)) {
+        return S21_FP_NINF;
+      }
+      return S21_FP_PINF;
+    }
+    if (s21_isnegative(base) && exp > 0) {
+      return 0.0L;
+    }
+  }
+  if (base == S21_FP_NINF) {
+    if (exp > 0.0) {
+      if (s21_isodd(exp)) {
+        return S21_FP_NINF;
+      }
+      return S21_FP_PINF;
+    }
+    if (exp < 0.0) {
+      return 0.0L;
+    }
+  }
+  if (s21_isnegative(base) && s21_isreal(exp)) {
+    return S21_FP_NAN;
+  }
+
+  int sign = 1;
+  if (s21_isnegative(base)) {
+    base = -base;
+    if (s21_isint(exp) && !s21_iseven(exp)) {
+      sign = -1;
+    }
+  }
+
+  long double res = sign * s21_exp(exp * s21_log(base));
+
+  return res;
 }
