407cd25ac6ba5464db8439194746f3218a2634ad
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-05-05 03:06:53 +0700

    sqrt: halfway ready

--- src/lib/s21_sqrt.c
+++ src/lib/s21_sqrt.c
@@ -1,18 +1,28 @@
+#include <math.h>
+
 #include "s21_fp_utils.h"
 #include "s21_math.h"
 
 long double s21_sqrt(double x) {
-  long double y = x;
-  long double y_temp = 1;
-  if (x < 0) {
-    y = -S21_FP_NAN;
-  } else if (x == S21_FP_PINF) {
-    y = S21_FP_PINF;
-  } else if (x > 0) {
-    while ((y_temp - y) || (y - y_temp) > S21_EPSILON) {
-      y = y_temp;
-      y_temp = (y_temp + (x / y_temp)) / 2;
+  if (x < 0.0) {
+    return S21_FP_NAN;
+  }
+  if (x == 0.0) {
+    return 0.0;
+  }
+  if (x == S21_FP_PINF) {
+    return S21_FP_PINF;
+  }
+
+  long double intermediate_value = x;
+  long double prev_value = 1;
+
+  if (x > 0) {
+    while (s21_fabs(intermediate_value - prev_value) > S21_EPSILON) {
+      intermediate_value = prev_value;
+      prev_value = (prev_value + (x / prev_value)) / 2;
     }
   }
-  return y;
+
+  return intermediate_value;
 }
