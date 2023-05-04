8ca894c7fa5315d0df8ab9057f05f6b09af5675c
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-05-04 15:52:21 +0700

    pow: function is decomposed in two functions

--- src/lib/s21_pow.c
+++ src/lib/s21_pow.c
@@ -1,57 +1,74 @@
 #include "s21_fp_utils.h"
 #include "s21_math.h"
 
+bool are_theese_boundary_values(double base, double exp, long double *res);
+
 long double s21_pow(double base, double exp) {
-  if (base == 1.0 || exp == 0.0) {
-    return 1.0L;
-  }
-  if (base == -1.0) {
-    if (s21_isinf(exp)) {
-      return 1.0L;
-    }
-    if (s21_fabs(exp) == DBL_MAX) {
-      return 1.0L;
-    }
-  }
-  if (base == 0.0) {
-    if (s21_ispositive(base) && exp < 0) {
-      return S21_FP_PINF;
-    }
-
-    if (s21_isnegative(base) && exp < 0) {
-      if (s21_isint(exp) && s21_isodd(exp)) {
-        return S21_FP_NINF;
-      }
-      return S21_FP_PINF;
-    }
-    if (s21_isnegative(base) && exp > 0) {
-      return 0.0L;
-    }
-  }
-  if (base == S21_FP_NINF) {
-    if (exp > 0.0) {
-      if (s21_isodd(exp)) {
-        return S21_FP_NINF;
-      }
-      return S21_FP_PINF;
-    }
-    if (exp < 0.0) {
-      return 0.0L;
-    }
-  }
-  if (s21_isnegative(base) && s21_isreal(exp)) {
-    return S21_FP_NAN;
+  long double res;
+  if (are_theese_boundary_values(base, exp, &res)) {
+    return res;  // NOLINT
   }
 
-  int sign = 1;
+  int res_sign = 1;
   if (s21_isnegative(base)) {
     base = -base;
     if (s21_isint(exp) && !s21_iseven(exp)) {
-      sign = -1;
+      res_sign = -1;
     }
   }
 
-  long double res = sign * s21_exp(exp * s21_log(base));
+  res = res_sign * s21_exp(exp * s21_log(base));
 
   return res;
+}
+
+// NOLINTNEXTLINE
+bool are_theese_boundary_values(double base, double exp, long double *res) {
+  bool values_are_boundary = true;
+
+  if (base != 1.0 && s21_isnan(exp)) {
+    *res = S21_FP_NAN;
+  } else if (base == 1.0 || exp == 0.0) {
+    *res = 1.0L;
+  } else if (base == -1.0) {
+    if (s21_isinf(exp)) {
+      *res = 1.0L;
+    } else if (s21_isint(exp) && s21_iseven(exp)) {
+      *res = 1.0L;
+    } else if (s21_isreal(exp)) {
+      *res = S21_FP_NAN;
+    } else {
+      return false;
+    }
+  } else if (base == 0.0) {
+    if (s21_ispositive(base) && exp < 0) {
+      *res = S21_FP_PINF;
+    } else if (s21_isnegative(base) && exp < 0) {
+      if (s21_isint(exp) && s21_isodd(exp)) {
+        *res = S21_FP_NINF;
+      } else {
+        *res = S21_FP_PINF;
+      }
+    } else if (s21_isnegative(base) && exp > 0) {
+      *res = 0.0L;
+    } else {
+      return false;
+    }
+  } else if (base == S21_FP_NINF) {
+    if (exp > 0.0) {
+      if (s21_isint(exp) && s21_isodd(exp)) {
+        *res = S21_FP_NINF;
+      } else {
+        *res = S21_FP_PINF;
+      }
+    } else {
+      *res = 0.0L;
+    }
+  } else if (s21_isnegative(base) && s21_isreal(exp)) {
+    *res = S21_FP_NAN;
+  } else {
+    values_are_boundary = false;
+  }
+
+  return values_are_boundary;
 }
