c8b3b4eceda14c97d52690d2049bae2dcbe403f6
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-05-05 02:03:19 +0700

    asin: completed

--- src/lib/s21_asin.c
+++ src/lib/s21_asin.c
@@ -3,11 +3,22 @@
 
 // through atan
 long double s21_asin(double x) {
+  const long double sin_of_pi_4 = 0.70710678118654746L;
+
+  if (x < -1.0 || 1.0 < x) {
+    return S21_FP_NAN;
+  }
   if (x == 1.0) {
-    return S21_PI / 2;
+    return S21_PI_2;  // NOLINT
   }
   if (x == -1.0) {
-    return -S21_PI / 2;
+    return -S21_PI_2;  // NOLINT
+  }
+  if (x == sin_of_pi_4) {
+    return S21_PI_4;
+  }
+  if (x == -sin_of_pi_4) {
+    return -S21_PI_4;
   }
 
   return s21_atan(x / s21_sqrt(1 - x * x));
