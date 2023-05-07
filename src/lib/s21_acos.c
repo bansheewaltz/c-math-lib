e3c45f6a2fd4639bb7dea1854e084ff89d17466e
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-05-07 22:08:46 +0700

    asin/acos: constants were changed in favor of corresponding system library references

--- src/lib/s21_acos.c
+++ src/lib/s21_acos.c
@@ -1,25 +1,26 @@
 #include "s21_fp_utils.h"
-#include "s21_fp_utilsl.h"
 #include "s21_math.h"
 
 // through atan
 long double s21_acos(double x) {
+  const double SYSTEMLIB_COS_PI_4 = 0.70710678118654752;
+
   if (x < -1.0 || 1.0 < x) {
     return S21_FP_NAN;
   }
-  if (x == 1.0) {
+  if (x == +1.0) {
     return 0.0L;
   }
   if (x == -1.0) {
     return S21_PI;
   }
-  if (x == 0) {
-    return S21_PI / 2;
+  if (x == 0.0) {
+    return S21_PI_2;
   }
-  if (s21_fabsl((long double)x - S21_SQRT1_2) < S21_EPSILON) {
+  if (x == +SYSTEMLIB_COS_PI_4) {
     return S21_PI_4;
   }
-  if (s21_fabsl((long double)x - S21_SQRT1_2) < S21_EPSILON) {
+  if (x == -SYSTEMLIB_COS_PI_4) {
     return 2 * S21_PI - S21_PI_4;
   }
   if (0.0 < x && x < 1.0) {
--- src/lib/s21_asin.c
+++ src/lib/s21_asin.c
@@ -1,22 +1,23 @@
 #include "s21_fp_utils.h"
-#include "s21_fp_utilsl.h"
 #include "s21_math.h"
 
 // through atan
 long double s21_asin(double x) {
+  const double SYSTEMLIB_SIN_PI_4 = 0.70710678118654746;
+
   if (x < -1.0 || 1.0 < x) {
-    return S21_FP_NAN;
+    return +S21_FP_NAN;
   }
-  if (x == 1.0) {
+  if (x == +1.0) {
     return +S21_PI_2;
   }
   if (x == -1.0) {
     return -S21_PI_2;
   }
-  if (s21_fabsl((long double)x - S21_SQRT1_2) < S21_EPSILON) {
+  if (x == +SYSTEMLIB_SIN_PI_4) {
     return +S21_PI_4;
   }
-  if (s21_fabsl((long double)x + S21_SQRT1_2) < S21_EPSILON) {
+  if (x == -SYSTEMLIB_SIN_PI_4) {
     return -S21_PI_4;
   }
 
